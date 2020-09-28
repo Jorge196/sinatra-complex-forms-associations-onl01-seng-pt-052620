de052a861ab134e0afe23c168c595f74e94f1d46
Jorge Ramirez <joramirez96@gmail.com>
2020-09-28 18:09:04 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,8 +23,11 @@ class PetsController < ApplicationController
   end
 
   get '/pets/:id/edit' do
-    @pet = Pet
-    
+    @pet = Pet.find_by_id(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end 
+
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
